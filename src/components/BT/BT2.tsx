1418ce4a330f51565605736db2bb1d4c97c3fb02
DatNguyen <thanhdat1510z03@gmail.com>
2024-09-27 18:08:32 +0700

    @Init: BT2

---
+++ src/components/BT/BT2.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const BT2 = () => {
+  return (
+    <React.Fragment>
+      <div className="h-[800px] w-[800px]">
+        <div className="flex">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
